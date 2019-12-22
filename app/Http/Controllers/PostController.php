521a9afea345dc0937fdfba6166915c5ed974146
Marko Radulovic <upss070288@gmail.com>
2019-12-23 00:23:25 +0100

    unit testing for user done

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use \App\Http\Resources\Post as PostResource;
+
+class PostController extends Controller
+{
+    public function store() {
+
+        $data = request()->validate([
+            'data.attributes.body' => '',
+        ]);
+
+        $post = request()->user()->posts()->create($data['data']['attributes']);
+
+        return new PostResource($post);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,10 @@
 <?php
 
 use Illuminate\Http\Request;
+Route::middleware('auth:api')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+    Route::post('/posts', 'PostController@store');
 });
