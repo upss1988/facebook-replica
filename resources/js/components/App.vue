07b91598d930ed589a2527b50860acf0a24a04d9
Marko Radulovic <upss070288@gmail.com>
2019-12-22 17:07:23 +0100

    app is ready for development

---
+++ resources/js/components/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>Start
+    <router-view></router-view></div>
+</template>
+
+<script>
+    export default {
+        name: "App"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/views/Start.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>sadfasdfsadf g</div>
+</template>
+
+<script>
+    export default {
+        name: "Start"
+    }
+</script>
+
+<style scoped>
+
+</style>
