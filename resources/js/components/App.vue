bd632f9667f051969fe8215eb8934e933b9be27e
Marko Radulovic <upss070288@gmail.com>
2019-12-23 10:02:52 +0100

    start with front end

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,11 +1,17 @@
 <template>
-    <div>Start
+    <div class="flex flex-col flex-1 h-screen overflow-y-hidden">
+        <Nav />
     <router-view></router-view></div>
 </template>
 
 <script>
+    import Nav from './Nav';
     export default {
         name: "App"
+
+        components: {
+            Nav
+        }
     }
 </script>
 
